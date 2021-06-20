9c8ea6cc741d74ddc83cac499ea52ae68ae97ba4
Yatskiv Kath <iamkate4@gmail.com>
2021-06-20 20:53:17 +0300

    fixed redirecting problems

--- src/Views/Home/HomeView.jsx
+++ src/Views/Home/HomeView.jsx
@@ -1,15 +1,19 @@
+/* eslint-disable react/prop-types */
 import Header from "../../modules/Header";
 import Sidebar from "../../modules/Sidebar";
 import Files from "../../modules/Files";
 
-const HomeView = () => (
-  <>
-    <Header />
-    <div className="app_main container">
-      <Sidebar />
-      <Files />
-    </div>
-  </>
-);
+const HomeView = ({ token }) => {
+  console.log(token);
+  return (
+    <>
+      <Header />
+      <div className="app_main container">
+        <Sidebar />
+        <Files />
+      </div>
+    </>
+  );
+};
 
 export default HomeView;
